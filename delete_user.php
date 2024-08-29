713c1daabc1552ec0399ee0444a01edf1e4d22e0
dancarubioo <132722845+dancarubioo@users.noreply.github.com>
2024-08-29 20:36:15 +0800

    Add files via upload

---
+++ delete_user.php
@@ -0,0 +1,10 @@
+<?php
+include "../user/connection.php";
+$id = $_GET["id"];
+mysqli_query($link, "DELETE FROM user_registration
+                    WHERE id = $id");
+?>
+
+<script type = "text/javascript">
+    window.location = "add_new_user.php";
+</script>
